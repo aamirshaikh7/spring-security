977e371678d4d7be8e988589379f2914b66c986b
Aamir Shaikh <aamirshaikh3232@outlook.com>
2022-11-30 20:37:51 +0530

    Downgrade Spring boot version

--- src/main/java/com/demo/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/demo/springsecurity/security/SecurityConfig.java
@@ -1,23 +1,20 @@
 package com.demo.springsecurity.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .anyRequest()
-                        .authenticated()
-                )
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
                 .httpBasic();
-
-        return http.build();
     }
 }
