dc994c98b730b875455711a3e64966f673d3109b
Aamir Shaikh <aamirshaikh3232@outlook.com>
2022-12-17 21:38:41 +0530

    Add successfulAuthentication() in JwtUsernameAndPasswordAuthenticationFilter

--- src/main/java/com/demo/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/demo/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,15 +1,21 @@
 package com.demo.springsecurity.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -22,7 +28,11 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
+//            map sent credentials to UsernameAndPasswordAuthenticationRequest
+
             UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+
+//            validate credentials
 
             Authentication authenticate = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
@@ -30,5 +40,24 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+//        generate token
+
+        String key = "cDPODrxs2S6BXhIcMX3OCpFVGIsmlWd6xN4KkgxP3cA=";
+
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(1)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+//        send token back to client
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
