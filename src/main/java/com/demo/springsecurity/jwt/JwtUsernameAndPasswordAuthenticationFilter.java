f7be3cd8d16f72ed007cbb6d81227b296b46bea3
Aamir Shaikh <aamirshaikh3232@outlook.com>
2022-12-17 18:10:01 +0530

    Add JwtUsernameAndPasswordAuthenticationFilter - attemptAuthentication

---
+++ src/main/java/com/demo/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.demo.springsecurity.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper().readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+
+            Authentication authenticate = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+            return authenticationManager.authenticate(authenticate);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
